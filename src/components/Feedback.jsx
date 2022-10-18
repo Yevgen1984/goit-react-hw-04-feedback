2b685014d6e1585e3f1ee40d1c479ef3d5e0ab17
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-10-18 16:10:15 +0300

    changes HW 4 (17.10.22)

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOption } from './FeedbackOptions/FeedbackOptions';
